d76ec03638a5ff5a344af81cafa3310b0cd30846
007 <evengeo@163.com>
2018-08-02 19:16:59 +0800

    初始化提交，修真院任务

---
+++ src/main/java/com/jiaoyang/App.java
@@ -0,0 +1,13 @@
+package com.jiaoyang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
